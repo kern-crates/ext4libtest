efff3562ebba195ce0fff22a7563edad7c597e14
yuoo655 <lenuulan@gmail.com>
2024-12-31 08:15:50 +0800

    feat: add some file open test

--- src/lib.rs
+++ src/lib.rs
@@ -92,42 +92,32 @@ mod tests {
         }
     }
 
-    // #[test]
-    // fn test_open() {
+    #[test]
+    fn test_open() {
 
-    //     let disk = Arc::new(Disk {});
-    //     let ext4 = Ext4::open(disk);
+        let disk = Arc::new(Disk {});
+        let ext4 = Ext4::open(disk);
 
-    //     let path = "";
-    //     let mut ext4_file = Ext4File::new();
-    //     let r = ext4.ext4_open_new(&mut ext4_file, path, "r+", false);
-    //     assert!(ext4_file.inode == 2);
-    //     assert!(r.is_ok(), "open directory error {:?}", r.err());
+        let path = ".";
+        let r = ext4.ext4_file_open(path, "r+");
+        assert!(r.unwrap() == 2);
+        assert!(r.is_ok(), "open directory error {:?}", r.err());
 
-    //     let path = ".";
-    //     let mut ext4_file = Ext4File::new();
-    //     let r = ext4.ext4_open_new(&mut ext4_file, path, "r+", false);
-    //     assert!(ext4_file.inode == 2);
-    //     assert!(r.is_ok(), "open directory error {:?}", r.err());
+        let path = "./";
+        let r = ext4.ext4_file_open(path, "r+");
+        assert!(r.unwrap() == 2);
+        assert!(r.is_ok(), "open directory error {:?}", r.err());
 
-    //     let path = "./";
-    //     let mut ext4_file = Ext4File::new();
-    //     let r = ext4.ext4_open_new(&mut ext4_file, path, "r+", false);
-    //     assert!(ext4_file.inode == 2);
-    //     assert!(r.is_ok(), "open directory error {:?}", r.err());
+        let path =
+            "test_files/dirtest0/./dirtest1/../dirtest1/../../dirtest0/dirtest1/dirtest2/dirtest3";
+        let r = ext4.ext4_file_open(path, "r+");
+        assert!(r.is_ok(), "open directory error {:?}", r.err());
 
-    //     let path =
-    //         "test_files/dirtest0/./dirtest1/../dirtest1/../../dirtest0/dirtest1/dirtest2/dirtest3";
-    //     let mut ext4_file = Ext4File::new();
-    //     let r = ext4.ext4_open_new(&mut ext4_file, path, "r+", false);
-    //     assert!(r.is_ok(), "open directory error {:?}", r.err());
+        let path = "test_files/dirtest0/./dirtest1/../dirtest1/../../dirtest0/dirtest1/dirtest2/dirtest3/nonexistpath";
+        let r = ext4.ext4_file_open(path, "r+");
+        assert!(r.is_err());
 
-    //     let path = "test_files/dirtest0/./dirtest1/../dirtest1/../../dirtest0/dirtest1/dirtest2/dirtest3/nonexistpath";
-    //     let mut ext4_file = Ext4File::new();
-    //     let r = ext4.ext4_open_new(&mut ext4_file, path, "r+", false);
-    //     assert!(r.is_err());
-
-    // }
+    }
 
     // #[test]
     // fn test_read_file() {
